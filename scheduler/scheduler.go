c183f39eb6804cd619ddc80bea5da2e090c9d413
6GOD <55304045+6ixGODD@users.noreply.github.com>
2024-04-22 22:17:32 +0200

    feature: scheduler interface

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,5 +1,32 @@
 package scheduler
 
 // TODO:
-// 	1. Task for clear logger cache and write to mongodb
-// 	2. Task for clear query cache
+// 	1. Tasks for clear logger cache in redis and write to mongodb
+// 	2. Tasks for clear query cache
+// 	3. Tasks for clear zap logger cache
+// 	4. Based on cron expression
+
+type Tasks interface {
+	ClearRedisLogCache()
+	ClearQueryCache()
+	SyncLogger()
+}
+
+type TasksImpl struct {
+}
+
+func NewScheduler() *TasksImpl {
+	return &TasksImpl{}
+}
+
+func (t *TasksImpl) ClearRedisLogCache() {
+	// TODO
+}
+
+func (t *TasksImpl) ClearQueryCache() {
+	// TODO
+}
+
+func (t *TasksImpl) SyncLogger() {
+	// TODO
+}
