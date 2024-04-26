44508c588e296fff38a5733cdfd63d7b495e967b
6GOD <55304045+6ixGODD@users.noreply.github.com>
2024-04-26 22:37:25 +0200

    todo: middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,13 +1,3 @@
 package middleware
 
 // JWT Middleware
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func JWT() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Next()
-	} // TODO: Implement JWT middleware
-}
--- middleware/logger.go
+++ middleware/logger.go
@@ -1,13 +1,3 @@
 package middleware
 
 // Logger Middleware
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func Logger() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Next()
-	} // TODO: Implement Logger middleware
-}
