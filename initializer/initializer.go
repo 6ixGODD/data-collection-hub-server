d33719747b7733e9b6d89e15fecb22eac212587b
6GOD <55304045+6ixGODD@users.noreply.github.com>
2024-04-26 15:16:45 +0200

    update: viper config

--- initializer/initializer.go
+++ initializer/initializer.go
@@ -1,32 +1,97 @@
 package initializer
 
 import (
-	"os"
+	"context"
 
 	"data-collection-hub-server/core/config"
+	"data-collection-hub-server/core/mongo"
+	"data-collection-hub-server/core/redis"
 	"data-collection-hub-server/core/zap"
-	"data-collection-hub-server/global"
-	"github.com/joho/godotenv"
+	"data-collection-hub-server/hooks"
+	"data-collection-hub-server/router"
+	"github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
-// TODO: Init global variables, db, wire, etc. return a app instance
-
-func InitApp() {
-	// Load .env file if exists
-	if envFile := os.Getenv("APP_ENV_FILE"); envFile != "" {
-		_ = godotenv.Load(envFile)
-	} else if _, err := os.Stat("../.env"); err == nil {
-		_ = godotenv.Load("../.env")
-	} else {
-		_ = godotenv.Load()
+// NewApp factory function that initializes the application and returns a fiber.App instance.
+func NewApp(appConfig config.Config) (*fiber.App, error) {
+	ctx := context.Background()
+	// Init Zap logger
+	if err := zap.InitLogger(&appConfig.ZapConfig); err != nil {
+		return nil, err
+	}
+	// Init MongoDB
+	if err := mongo.InitMongo(ctx, &appConfig.MongoConfig); err != nil {
+		return nil, err
+	}
+	// Init Redis
+	if err := redis.InitRedis(ctx, &appConfig.RedisConfig); err != nil {
+		return nil, err
 	}
 
-	// TODO: Init global variables
-	global.CONFIG = config.GetConfig()
-	global.LOGGER = zap.GetLogger()
-
 	// TODO: Init wire
-	// TODO: Init db
 
-	// TODO: return app instance
+	app := fiber.New(fiber.Config{
+		Prefork:                 appConfig.FiberConfig.Prefork,
+		ServerHeader:            appConfig.FiberConfig.ServerHeader,
+		BodyLimit:               appConfig.FiberConfig.BodyLimit,
+		Concurrency:             appConfig.FiberConfig.Concurrency,
+		ReadTimeout:             appConfig.FiberConfig.ReadTimeout,
+		WriteTimeout:            appConfig.FiberConfig.WriteTimeout,
+		IdleTimeout:             appConfig.FiberConfig.IdleTimeout,
+		ReadBufferSize:          appConfig.FiberConfig.ReadBufferSize,
+		WriteBufferSize:         appConfig.FiberConfig.WriteBufferSize,
+		ProxyHeader:             appConfig.FiberConfig.ProxyHeader,
+		DisableStartupMessage:   appConfig.FiberConfig.DisableStartupMessage,
+		AppName:                 appConfig.BaseConfig.AppName,
+		ReduceMemoryUsage:       appConfig.FiberConfig.ReduceMemoryUsage,
+		EnableTrustedProxyCheck: appConfig.FiberConfig.EnableTrustedProxyCheck,
+		TrustedProxies:          appConfig.FiberConfig.TrustedProxies,
+		EnablePrintRoutes:       appConfig.FiberConfig.EnablePrintRoutes,
+		JSONDecoder:             json.Unmarshal,
+		JSONEncoder:             json.Marshal,
+	})
+
+	// Register limiter middleware
+	app.Use(limiter.New(limiter.Config{
+		Max:               appConfig.LimiterConfig.Max,
+		Expiration:        appConfig.LimiterConfig.Expiration,
+		LimiterMiddleware: limiter.SlidingWindow{},
+		// LimitReached: nil,
+	}))
+
+	// Register hooks
+	app.Hooks().OnShutdown(
+		func() error {
+			return hooks.Shutdown(ctx, app)
+		},
+	)
+
+	// Ping
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
+	})
+
+	// Register routers
+	router.RegisterRouter(app)
+
+	return app, nil
+}
+
+// Run function that initializes the application and starts the server.
+func Run(config config.Config, appHost string, appPort string) error {
+	app, err := NewApp(config)
+	if err != nil {
+		return err
+	}
+
+	if appHost == "" {
+		appHost = config.BaseConfig.AppHost
+	}
+	if appPort == "" {
+		appPort = config.BaseConfig.AppPort
+	}
+
+	return app.Listen(appHost + ":" + appPort)
 }
--- test/initializer/initializer_test.go
+++ test/initializer/initializer_test.go
@@ -1,20 +1,35 @@
 package initializer
 
 import (
-	"os"
 	"testing"
 
-	"data-collection-hub-server/initializer"
+	"data-collection-hub-server/core/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestInitializer(t *testing.T) {
 	a := assert.New(t)
-	initializer.InitApp()
-	// Environment variables
-	a.NotNil(os.Getenv("APP_PORT"))
-	a.Equal("3000", os.Getenv("APP_PORT"))
-	a.NotNil(os.Getenv("APP_NAME"))
-	a.NotNil(os.Getenv("APP_VERSION"))
-	a.NotNil(os.Getenv("APP_HOST"))
+	// ctx := context.Background()
+	// app, err := initializer.NewApp(ctx)
+	// if err != nil {
+	// 	return
+	// }
+	// // Environment variables
+	// a.NotNil(os.Getenv("APP_PORT"))
+	// a.Equal("3000", os.Getenv("APP_PORT"))
+	// a.NotNil(os.Getenv("APP_NAME"))
+	// a.NotNil(os.Getenv("APP_VERSION"))
+	// a.NotNil(os.Getenv("APP_HOST"))
+	// err = app.Listen(":3000")
+	// if err != nil {
+	// 	return
+	// }
+	cfg, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	} else {
+		a.NotNil(cfg)
+		t.Logf("config: %+v", cfg)
+	}
+
 }
